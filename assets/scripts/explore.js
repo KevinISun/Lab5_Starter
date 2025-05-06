6fb0e2ef391104625475991f8523ebba63e87aa6
ttai2023 <territai0831@gmail.com>
2025-05-05 22:15:59 -0700

    started explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,26 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const synth = window.speechSynthesis;
+  const voiceSelect = document.getElementById('voice-select');
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+
+    for (let i = 0; i < voices.length ; i++) {
+      const option = document.createElement('option');
+      option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+      if (voices[i].default) {
+        option.textContent += ' -- DEFAULT';
+      }
+
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+  voiceSelect.addEventListener('change', handleHornChange);
+  populateVoiceList();
+
 }
