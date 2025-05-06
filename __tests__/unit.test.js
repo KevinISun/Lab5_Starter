75245a3d5a5102283549ddfb6c32bcda4ba60fb9
KevinISun <114264552+KevinISun@users.noreply.github.com>
2025-05-05 23:09:25 -0700

    valid test cases

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,28 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+test('Valid phone numbers', () => {
+  expect(isPhoneNumber('(123) 456-7890')).toBe(true);
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('Valid emails', () => {
+  expect(isEmail('whaevner@we.com')).toBe(true);
+  expect(isEmail('owie@google.com')).toBe(true);
+});
+
+test('Valid strong passwords', () => {
+  expect(isStrongPassword('a1b2c3d4')).toBe(true);
+  expect(isStrongPassword('A1234abcd')).toBe(true);
+});
+
+test('Valid dates', () => {
+  expect(isDate('12/31/2020')).toBe(true);
+  expect(isDate('1/1/2021')).toBe(true);
+});
+
+test('Valid hex colors', () => {
+  expect(isHexColor('#123abc')).toBe(true);
+  expect(isHexColor('#123')).toBe(true);
+});
+
