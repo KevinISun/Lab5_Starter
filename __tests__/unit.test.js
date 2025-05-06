0b0ad761cebe920e9522f6ca30b281251f05c08d
KevinISun <114264552+KevinISun@users.noreply.github.com>
2025-05-05 23:09:39 -0700

    valid test cases

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,28 +8,39 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-test('Valid phone numbers', () => {
+test('Valid phone number 1', () => {
   expect(isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+test('Valid phone number 2', () => {
   expect(isPhoneNumber('123-456-7890')).toBe(true);
 });
 
-test('Valid emails', () => {
+test('Valid email 1', () => {
   expect(isEmail('whaevner@we.com')).toBe(true);
+});
+test('Valid email 2', () => {
   expect(isEmail('owie@google.com')).toBe(true);
 });
 
-test('Valid strong passwords', () => {
+test('Valid strong password 1', () => {
   expect(isStrongPassword('a1b2c3d4')).toBe(true);
+});
+test('Valid strong password 2', () => {
   expect(isStrongPassword('A1234abcd')).toBe(true);
 });
 
-test('Valid dates', () => {
+test('Valid date 1', () => {
   expect(isDate('12/31/2020')).toBe(true);
+});
+test('Valid date 2', () => {
   expect(isDate('1/1/2021')).toBe(true);
 });
 
-test('Valid hex colors', () => {
+test('Valid hex colors 1', () => {
   expect(isHexColor('#123abc')).toBe(true);
-  expect(isHexColor('#123')).toBe(true);
 });
+test('Valid hex colors 2', () => {
+expect(isHexColor('#123')).toBe(true);
+});
+
 
