b2f241025d85b5222495a7d62fd60fe5efe3e7c0
ttai2023 <territai0831@gmail.com>
2025-05-05 23:16:12 -0700

    finished tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -15,11 +15,25 @@ test('Valid phone number 2', () => {
   expect(isPhoneNumber('123-456-7890')).toBe(true);
 });
 
+test('Valid phone number 3', () => {
+  expect(isPhoneNumber('1234567890')).toBe(false);
+});
+
+test('Valid phone number 4', () => {
+  expect(isPhoneNumber('123456--7890')).toBe(false);
+});
+
 test('Valid email 1', () => {
   expect(isEmail('whaevner@we.com')).toBe(true);
 });
 test('Valid email 2', () => {
   expect(isEmail('owie@google.com')).toBe(true);
+});
+test('Valid email 3', () => {
+  expect(isEmail('hello')).toBe(false);
+});
+test('Valid email 4', () => {
+  expect(isEmail('hello@@gmail.com')).toBe(false);
 });
 
 test('Valid strong password 1', () => {
@@ -28,6 +42,12 @@ test('Valid strong password 1', () => {
 test('Valid strong password 2', () => {
   expect(isStrongPassword('A1234abcd')).toBe(true);
 });
+test('Valid strong password 3', () => {
+  expect(isStrongPassword('1')).toBe(false);
+});
+test('Valid strong password 4', () => {
+  expect(isStrongPassword('1234567890123456')).toBe(false);
+});
 
 test('Valid date 1', () => {
   expect(isDate('12/31/2020')).toBe(true);
@@ -35,12 +55,25 @@ test('Valid date 1', () => {
 test('Valid date 2', () => {
   expect(isDate('1/1/2021')).toBe(true);
 });
+test('Valid date 3', () => {
+  expect(isDate('123456')).toBe(false);
+});
+test('Valid date 4', () => {
+  expect(isDate('1/1/20212')).toBe(false);
+});
+
 
 test('Valid hex colors 1', () => {
   expect(isHexColor('#123abc')).toBe(true);
 });
 test('Valid hex colors 2', () => {
 expect(isHexColor('#123')).toBe(true);
+});
+test('Valid hex colors 3', () => {
+  expect(isHexColor('23abc')).toBe(false);
+});
+test('Valid hex colors 4', () => {
+  expect(isHexColor('#123ab')).toBe(false);
 });
 
 
