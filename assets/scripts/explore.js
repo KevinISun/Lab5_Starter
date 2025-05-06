94e1be48018fb4b0776f1aadead80c6a4e66a325
ttai2023 <territai0831@gmail.com>
2025-05-05 22:34:31 -0700

    added voice

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -6,6 +6,9 @@ function init() {
   // TODO
   const synth = window.speechSynthesis;
   const voiceSelect = document.getElementById('voice-select');
+  const talkButton = document.querySelector('#explore > button'); 
+  const textToSpeak = document.getElementById('text-to-speak');
+  let selectedVoice = voiceSelect.value;
   let voices = []
 
   function populateVoiceList() {
@@ -24,13 +27,25 @@ function init() {
       voiceSelect.appendChild(option);
     }
   }
-  
+
+  function handleTalkButton() {
+    if (selectedVoice !== 'select') {
+        const utterance = new SpeechSynthesisUtterance(textToSpeak.value);
+        utterance.voice = voices.find(voice => voice.name === selectedVoice);
+        synth.speak(utterance);
+    }
+  }
+
   if (speechSynthesis.onvoiceschanged !== undefined) {
     speechSynthesis.onvoiceschanged = populateVoiceList;
   }
 
-  voiceSelect.addEventListener('change', handleHornChange);
   populateVoiceList();
-  
 
+  voiceSelect.addEventListener('change', () => {
+    selectedVoice = voiceSelect.value;
+  });
+
+  talkButton.addEventListener('click', handleTalkButton);
+  
 }
